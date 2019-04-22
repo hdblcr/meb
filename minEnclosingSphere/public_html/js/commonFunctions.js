86e5a98c8601e751b7ee44205c9f0f4e47c6e0b5
Luke Johnson <onlylukejohnson@gmail.com>
2019-04-22 11:43:56 -0400

    Convert three point and scalar mult

--- minEnclosingSphere/public_html/js/commonFunctions.js
+++ minEnclosingSphere/public_html/js/commonFunctions.js
@@ -2,8 +2,10 @@ function distance_between_points_squared(point1, point2) {
     return Math.pow(point1.x - point2.x, 2) + Math.pow(point1.y - point2.y, 2);
 }
 
-function scalar_multiply(vector, scalar) {
-    return [vector[0] * scalar, vector[1] * scalar];
+function scalar_multiply(line, scalar) {
+    var new_endpoint = new Point(scalar * line.endpoint.x, scalar * line.endpoint.y);
+    var new_line = new Line(startpoint, new_endpoint);
+    return new_line;
 }
 
 function midpoint(point1, point2) {
@@ -19,7 +21,18 @@ function circle_two_points(point1, point2) {
 }
 
 function circle_three_points(point1, point2, point3) {
-    // this is going to be less fun to do than I'd hope...
+    var center_x = ((math.pow(point1.x,2) + math.pow(point1.y,2))*(point2.y - point3.y) + 
+                (math.pow(point2.x,2) + math.pow(point2.y,2))*(point3.y - point1.y) + 
+                (math.pow(point3.x,2) + math.pow(point3.y,2))*(point1.y - point2.y)) /
+                (2 * (point1.x *(point2.y - point3.y) + point2.x * (point3.y - point1.y) + point3.x * (point1.y - point2.y)));
+    var center_y = ((math.pow(point1.x,2) + math.pow(point1.y,2))*(point3.x - point2.x) + 
+                (math.pow(point2.x,2) + math.pow(point2.y,2))*(point1.x - point3.x) + 
+                (math.pow(point3.x,2) + math.pow(point3.y,2))*(point2.x - point1.x)) /
+                (2 * (point1.x *(point2.y - point3.y) + point2.x * (point3.y - point1.y) + point3.x * (point1.y - point2.y)));
+    var center = new Point(center_x, center_y)
+    var radius = math.sqrt(distance_between_points_squared(center, point1));
+    var return_circle = new Circle(center, radius)
+    return return_circle;
 }
 
 function find_max_distance(point, list) {
