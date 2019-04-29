e3edd9bd30a2a912365ede43bb062ad8b8dd16d6
Sarah Peck <sarah.peck@uconn.edu>
2019-04-29 19:57:41 -0400

    getting started on graphics

--- js/drawingFunctions.js
+++ js/drawingFunctions.js
@@ -1,10 +1,23 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-function drawPoint(x, y) {
+ var w = 500;
+ var h = 500;
+
+ var svg = d3.select("body").append("svg");
+ svg.attr("width", w)
+    .attr("height", h);
+
+function drawObject(obj) {
+  if (obj.type === 'point') {
+    drawPoint(obj);
+  };
+}
+
+function drawPoint(obj) {
     var pt = new d3.symbol;
-    
+    svg.selectAll()
 }
--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+var center = new Point(50, 50);
+var circle = new Circle(center, 30);
+drawObject(circle);
 
-google.charts.load('current', {'packages':['corechart']});
-//          google.charts.setOnLoadCallback(drawChart);
-          
           function randomPoint() {
             var newval = Math.random() * 500;
             return newval;
@@ -34,14 +34,14 @@ google.charts.load('current', {'packages':['corechart']});
             var chart = new google.visualization.ScatterChart(document.getElementById('buncha_points'));
 
             chart.draw(data, options);
-          }          
-          
+          }
+
           function drawChart2() {
             var data = new google.visualization.DataTable();
             data.addColumn('number');
             data.addColumn('number');
 //            data.addRow([randomPoint(), randomPoint()]);
-                        
+
             var options = {
               legend: 'none',
               colors: ['#087037'],
@@ -54,27 +54,27 @@ google.charts.load('current', {'packages':['corechart']});
               hAxis: {title: 'X', minValue: 0, maxValue: 100},
               vAxis: {title: 'Y', minValue: 0, maxValue: 100}
             };
-            
+
             var chart = new google.visualization.ScatterChart(document.getElementById('buncha_points_delay'));
 
             // Start the animation by listening to the first 'ready' event.
-            google.visualization.events.addOneTimeListener(chart, 'ready', addPoint);    
+            google.visualization.events.addOneTimeListener(chart, 'ready', addPoint);
             chart.draw(data, options);
-            
+
             // Pause with click
 //            google.visualization.events.addListener(chart, 'click', pause);
-            
+
 //             Control all other animations by listening to the 'animationfinish' event.
 //            google.visualization.events.addListener(chart, 'animationfinish', addPoint);
-            
+
             google.visualization.events.addListener(chart, 'click', addPoint);
-            
+
             function addPoint() {
               data.addRow([randomPoint(), randomPoint()]);
               chart.draw(data, options);
             }
-          }          
-          
+          }
+
           function startDraw() {
               drawChart3();
               setTimeout(endDraw, 120000);
@@ -83,18 +83,18 @@ google.charts.load('current', {'packages':['corechart']});
           function endDraw() {
               drawChart3();
           }
-          
+
           function handler(go) {
               if (go) {
-                  
+
               }
           }
-          
+
           function drawChart3() {
             var data = new google.visualization.DataTable();
             data.addColumn('number');
             data.addColumn('number');
-                        
+
             var options = {
               legend: 'none',
               colors: ['#087037'],
@@ -107,22 +107,22 @@ google.charts.load('current', {'packages':['corechart']});
               hAxis: {title: 'X', minValue: 0, maxValue: 100},
               vAxis: {title: 'Y', minValue: 0, maxValue: 100}
             };
-            
+
             var chart = new google.visualization.ScatterChart(document.getElementById('auto_till_stop'));
 
             // Start the animation by listening to the first 'ready' event.
-            google.visualization.events.addOneTimeListener(chart, 'ready', addPoint);    
+            google.visualization.events.addOneTimeListener(chart, 'ready', addPoint);
             chart.draw(data, options);
-            
+
             // this is the part the handler should do
             google.visualization.events.addListener(chart, 'click', addPoint);
-            
+
             function addPoint() {
               data.addRow([randomPoint(), randomPoint()]);
               chart.draw(data, options);
             }
           }
-          
+
           function svg1() {
               var point = new Point(randomPoint(), randomPoint());
               var newString = "<svg width='500' height='500'>";
@@ -132,4 +132,3 @@ google.charts.load('current', {'packages':['corechart']});
               document.getElementById('svg1').innerHTML = newString;
               window.addEventListener("DOMContentLoaded", initialize);
           }
-          
